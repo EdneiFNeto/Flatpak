75e37067ffb9d2710fa502a76bae5851c7de66f3
Ednei <edneifneto@gmail.com>
2024-09-16 11:45:04 -0300

    Add server .jar

--- src/run-java.sh
+++ src/run-java.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-exec java -jar /app/bin/my-java-app.jar
+exec java -jar /app/bin/ZoopPC-Server-jvm.jar
 
 # Definir permissões para o arquivo .jar
-echo "Definindo permissões para my-java-app.jar..."
-chmod 644 my-java-app.jar
+echo "Definindo permissões para ZoopPC-Server.jar..."
+chmod 644 ZoopPC-Server.jar
 # Definir permissões para o script de execução
 echo "Definindo permissões para run-java.sh..."
 chmod +x run-java.sh
