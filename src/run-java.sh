372f08bc5baf6f0a11fc8dc44b3899dcf910815b
Ednei <edneifneto@gmail.com>
2024-09-16 12:00:37 -0300

    Update command  and sh

--- src/run-java.sh
+++ src/run-java.sh
@@ -3,7 +3,7 @@ exec java -jar /app/bin/ZoopPC-Server-jvm.jar
 
 # Definir permissões para o arquivo .jar
 echo "Definindo permissões para ZoopPC-Server.jar..."
-chmod 644 ZoopPC-Server.jar
+chmod 644 ZoopPC-Server-jvm.jar
 # Definir permissões para o script de execução
 echo "Definindo permissões para run-java.sh..."
 chmod +x run-java.sh
